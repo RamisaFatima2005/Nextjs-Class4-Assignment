c3e94bda2fc40e480d0869aac9cf0c26194e2c08
RamisaFatima2005 <163189004+RamisaFatima2005@users.noreply.github.com>
2024-10-03 15:18:42 +0500

    Student ID Card

---
+++ app/components/Card.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface Idcard{
+    name:string,
+    rollNo:number,
+    center:string,
+    camp:string,
+    day:string,
+    time:string,
+}
+
+function Card(props:Idcard) {
+  return (
+    <>
+    <div className="rounded-2xl p-5 m-5 bg-slate-300 font-sans m-20 max-w-96 h-72 border-4 border-black">
+    <img src="https://www.governorsindh.com/logo.png" className="h-14 w-12" />
+        <h1 className="text-blue-900 font-bold">Name: <span className='text-black font-semibold'>{props.name}</span></h1>
+        <h1 className="text-blue-900 font-bold">Roll No: <span className='text-black font-semibold'>{props.rollNo}</span></h1>
+        <h1 className="text-blue-900 font-bold">Center: <span className='text-black font-semibold'>{props.center}</span></h1>
+        <h1 className="text-blue-900 font-bold">Campus: <span className='text-black font-semibold'>{props.camp}</span></h1>
+        <h1 className="text-blue-900 font-bold">Day: <span className='text-black font-semibold'>{props.day}</span></h1>
+        <h1 className="text-blue-900 font-bold">Time: <span className='text-black font-semibold'>{props.time}</span></h1>
+        <p className='bg-cyan-950 text-white'>batch1</p>
+        <h1 className='text-blue-950 font-bold'>Q2</h1>
+    </div>
+    </>
+  )
+}
+
+export default Card
---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Card from "./components/Card";
+
+export default function Home() {
+  return (
+    <div>
+    <Card
+     name='Ramisa Fatima' 
+     rollNo= {286493}
+     center='Governor House Karachi' 
+     camp='Main' 
+     day='Monday' 
+     time='02:00PM-05:00PM'
+     />
+
+<Card
+     name='Sidra Farooq' 
+     rollNo= {345803}
+     center='Governor House Karachi' 
+     camp='Main' 
+     day='Monday' 
+     time='02:00PM-05:00PM'/>
+
+<Card
+     name='Damia Nigar' 
+     rollNo= {448903}
+     center='Governor House Karachi' 
+     camp='Main' 
+     day='Monday' 
+     time='02:00PM-05:00PM'/>
+    </div>
+  );
+}
